35ff384f1072bd1f61b07b4d56e32f19d5a843d2
Umut Benzer <umutbenzer@gmail.com>
2015-09-12 10:09:33 +0300

    Add destroy functionality to sidenav
    Unstable and untested implementation of sidebar, which exposes a
    destroy function as a part of the API that can be used to gracefully
    remove sidenav functionality from the page.

--- src/sidenav.js
+++ src/sidenav.js
@@ -8,43 +8,19 @@ var html = doc.documentElement;
 require("es6-promise").polyfill(); // Shim for browsers that does not support Promise natively.
 
 function Sidenav(options) {
-  options = options || {};
-
-  this.extraClosePixels = options.extraClosePixels || 30;
-  this.width = options.width || 350;
-  this.sidenavOpacity = options.sidenavOpacity || 0.5;
-
-  this.isBusy = false; // Is there a progress? Loading or closing?
-  this.isOpened = false;
-  this.currentOpacity = 0;
-  this.currentWidth = 0;
-
-  // Sets panel
-  this.sidenav = options.sidenav;
-  this.backdrop = options.backdrop;
-  this.content = options.content;
-
-  // Sets classnames
-  this.content.classList.add("sn-content");
-  this.sidenav.classList.add("sn-sidenav");
-  setTransformX(this.sidenav, (-1 * this.width) - this.extraClosePixels);
-  this.backdrop.classList.add("sn-backdrop");
-
-  // Sets events
-  this.initEvents();
+  init.call(this, options);
 }
 
 Sidenav.prototype.open = function() {
   var self = this;
 
-  if (self.isBusy) { return Promise.reject(); }
-  self.isBusy = true;
+  if (self.openClosePromise !== null) { return Promise.reject(); }
 
   html.classList.add("sn-visible");
 
-  var promise = this.showHideSidebarBackdrop(true);
-  return promise.then(function() {
-    self.isBusy = false;
+  self.openClosePromise = showHideSidebarBackdrop.call(self, true);
+  return self.openClosePromise.then(function() {
+    self.openClosePromise = null;
     self.isOpened = true;
   });
 };
@@ -52,19 +28,165 @@ Sidenav.prototype.open = function() {
 Sidenav.prototype.close = function() {
   var self = this;
 
-  if (self.isBusy) { return Promise.reject(); }
-  self.isBusy = true;
+  if (self.openClosePromise !== null) { return Promise.reject(); }
 
-  var promise = this.showHideSidebarBackdrop(false);
-  return promise
+  self.openClosePromise = showHideSidebarBackdrop.call(self, false);
+  return self.openClosePromise
     .then(function() {
-      self.isBusy = false;
+      self.openClosePromise = null;
       self.isOpened = false;
       html.classList.remove("sn-visible");
     });
 };
 
-Sidenav.prototype.expandTo = function(px) {
+Sidenav.prototype.destroy = function() {
+  // remove event bindings
+  doc.addEventListener("touchstart", touchstart);
+  doc.addEventListener("touchmove", touchmove);
+  doc.addEventListener("touchcancel", touchfinish);
+  doc.addEventListener("touchend", touchfinish);
+
+  // reset everything to initial state
+  // please note that destroy will not restore everything 1:1
+  // it won't keep inline styles, if there was before sidenav init
+  // it will just delete sn-* classes even they exist before init
+  // if you need more complicated logic to keep exactly the same behaviour
+  // open a PR
+  self.content.classList.remove("sn-content");
+  self.sidenav.classList.remove("sn-sidenav");
+  self.backdrop.classList.remove("sn-backdrop");
+  html.classList.remove("sn-visible");
+  removeTransformX(self.sidenav);
+
+  // remove dom references
+  // normally we don't need these because user should
+  // remove reference to sidenav instance in his code,
+  // but just in case if he forgots...
+  self.sidenav = null;
+  self.backdrop = null;
+  self.content = null;
+}
+
+function init(options) {
+  var self = this;
+  var fingerId = null;
+  var startX = null;
+  var startY = null;
+  var startWidth = null;
+  var wasInSidenavOnce = false;
+  var horizontalLock = false;
+  var previousX = null;
+  var previousY = null;
+
+  options = options || {};
+
+  // used to prevent sidenav's shadow to be seen when it is closed
+  self.extraClosePixels = options.extraClosePixels || 30;
+  self.width = options.width || 350;
+  self.sidenavOpacity = options.sidenavOpacity || 0.5;
+
+  self.openClosePromise = null; // Is there a progress? Loading or closing?
+  self.isOpened = false;
+  self.currentOpacity = 0;
+  self.currentWidth = 0;
+
+  // Sets panel
+  self.sidenav = options.sidenav;
+  self.backdrop = options.backdrop;
+  self.content = options.content;
+
+  // Sets classnames
+  self.content.classList.add("sn-content");
+  self.sidenav.classList.add("sn-sidenav");
+  setTransformX(self.sidenav, (-1 * self.width) - self.extraClosePixels);
+  self.backdrop.classList.add("sn-backdrop");
+
+  self.backdrop.addEventListener("click", function() {
+    self.close();
+  });
+
+  doc.addEventListener("touchstart", touchstart);
+};
+
+function touchstart(e) {
+  var self = this;
+  if (self.openClosePromise !== null) { return; }
+  if (fingerId !== null) { return; }
+
+  if (e.touches.length !== 1) { return; }
+
+  // if closed, check if the touch is from the left edge of the screen
+  if (!self.isOpened && e.touches[0].clientX > 10) { return; }
+
+  fingerId = e.touches[0].identifier;
+  startX = e.touches[0].clientX;
+  startY = e.touches[0].clientY;
+  startWidth = self.currentWidth;
+  wasInSidenavOnce = false;
+  horizontalLock = false;
+  previousX = -999;
+  previousY = -999;
+
+  // bind touchmove and end cancel events
+  doc.addEventListener("touchmove", touchmove);
+  doc.addEventListener("touchcancel", touchfinish);
+  doc.addEventListener("touchend", touchfinish);
+}
+
+function touchmove(e) {
+  var self = this;
+  for (var i = 0; i < e.changedTouches.length; i++) {
+    if (fingerId === e.changedTouches[i].identifier) {
+      if (Math.abs(e.changedTouches[i].clientX - previousX) < 1 &&
+          Math.abs(e.changedTouches[i].clientY - previousY) < 1) {
+        // Do not over invoke move event
+        return;
+      }
+      previousX = e.changedTouches[i].clientX;
+      previousY = e.changedTouches[i].clientY;
+
+      if (self.isOpened) {
+        if (!horizontalLock && Math.abs(startX - e.changedTouches[i].clientX) < Math.abs(startY - e.changedTouches[i].clientY)) {
+          doTouchFinish(null);
+          return;
+        }
+
+        horizontalLock = true;
+
+        // check if direction is horizontal, check if are on sidenav
+        if (!wasInSidenavOnce && e.changedTouches[i].clientX > self.currentWidth) { return; }
+      }
+      wasInSidenavOnce = true;
+
+      self.expandTo(startWidth + (e.changedTouches[i].clientX - Math.min(startX, self.width)));
+      return;
+    }
+  }
+}
+
+function touchfinish(e) {
+  var self = this;
+  for (var i = 0; i < e.changedTouches.length; i++) {
+    if (fingerId === e.changedTouches[i].identifier) {
+      doTouchFinish(wasInSidenavOnce ? self.currentWidth > self.width / 2 : null);
+    }
+  }
+}
+
+function doTouchFinish(shouldOpen) {
+  var self = this;
+  if (shouldOpen === true) {
+    self.open();
+  } else if (shouldOpen === false) {
+    self.close();
+  }
+  doc.removeEventListener("touchmove", touchmove);
+  doc.removeEventListener("touchcancel", touchfinish);
+  doc.removeEventListener("touchend", touchstart);
+  fingerId = null;
+}
+
+function expandTo(px) {
   var self = this;
 
   px = Math.min(px, self.width);
@@ -78,7 +200,7 @@ Sidenav.prototype.expandTo = function(px) {
   self.currentWidth = px;
 };
 
-Sidenav.prototype.showHideSidebarBackdrop = function(show) {
+function showHideSidebarBackdrop(show) {
   var self = this;
 
   var promise = new Promise(function(resolve) {
@@ -124,102 +246,16 @@ Sidenav.prototype.showHideSidebarBackdrop = function(show) {
   return promise;
 };
 
-Sidenav.prototype.initEvents = function() {
-  var self = this;
-  var fingerId = null;
-  var startX = null;
-  var startY = null;
-  var startWidth = null;
-  var wasInSidenavOnce = false;
-  var horizontalLock = false;
-  var previousX = null;
-  var previousY = null;
-
-  self.backdrop.addEventListener("click", function() {
-    self.close();
-  });
-
-  doc.addEventListener("touchstart", touchstart);
-
-  function touchstart(e) {
-    if (self.isBusy) { return; }
-    if (fingerId !== null) { return; }
-
-    if (e.touches.length !== 1) { return; }
-
-    // if closed, check if the touch is from the left edge of the screen
-    if (!self.isOpened && e.touches[0].clientX > 10) { return; }
-
-    fingerId = e.touches[0].identifier;
-    startX = e.touches[0].clientX;
-    startY = e.touches[0].clientY;
-    startWidth = self.currentWidth;
-    wasInSidenavOnce = false;
-    horizontalLock = false;
-    previousX = -999;
-    previousY = -999;
-
-    // bind touchmove and end cancel events
-    doc.addEventListener("touchmove", touchmove);
-    doc.addEventListener("touchcancel", touchfinish);
-    doc.addEventListener("touchend", touchfinish);
-  }
-
-  function touchmove(e) {
-    for (var i = 0; i < e.changedTouches.length; i++) {
-      if (fingerId === e.changedTouches[i].identifier) {
-        if (Math.abs(e.changedTouches[i].clientX - previousX) < 1 &&
-            Math.abs(e.changedTouches[i].clientY - previousY) < 1) {
-          // Do not over invoke move event
-          return;
-        }
-        previousX = e.changedTouches[i].clientX;
-        previousY = e.changedTouches[i].clientY;
-
-        if (self.isOpened) {
-          if (!horizontalLock && Math.abs(startX - e.changedTouches[i].clientX) < Math.abs(startY - e.changedTouches[i].clientY)) {
-            doTouchFinish(null);
-            return;
-          }
-
-          horizontalLock = true;
-
-          // check if direction is horizontal, check if are on sidenav
-          if (!wasInSidenavOnce && e.changedTouches[i].clientX > self.currentWidth) { return; }
-        }
-        wasInSidenavOnce = true;
-
-        self.expandTo(startWidth + (e.changedTouches[i].clientX - Math.min(startX, self.width)));
-        return;
-      }
-    }
-  }
-
-  function touchfinish(e) {
-    for (var i = 0; i < e.changedTouches.length; i++) {
-      if (fingerId === e.changedTouches[i].identifier) {
-        doTouchFinish(wasInSidenavOnce ? self.currentWidth > self.width / 2 : null);
-      }
-    }
-  }
-
-  function doTouchFinish(shouldOpen) {
-    if (shouldOpen === true) {
-      self.open();
-    } else if (shouldOpen === false) {
-      self.close();
-    }
-    doc.removeEventListener("touchmove", touchmove);
-    doc.removeEventListener("touchcancel", touchfinish);
-    doc.removeEventListener("touchend", touchstart);
-    fingerId = null;
-  }
-};
-
 function setTransformX(domEl, px) {
   domEl.style.transform = "translate3d(" + px + "px, 0, 0)";
   domEl.style["-webkit-transform"] = "translate3d(" + px + "px, 0, 0)";
 }
+
+function removeTransformX(domEl) {
+  delete domEl.style.transform;
+  delete domEl.style["-webkit-transform"];
+}
+
 function easeOutQuad(t, b, c, d) {
   t /= d;
   return -c * t * (t - 2) + b;
